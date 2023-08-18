9cccd98d5b85b5a653464da3c8fc9faf6aecff58
Raphael <oOraph@users.noreply.github.com>
2023-08-18 10:04:04 +0200

    wip
    Signed-off-by: Raphael <oOraph@users.noreply.github.com>

--- ec2_script.sh
+++ ec2_script.sh
@@ -41,17 +41,22 @@ sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-mkdir /shared
+sudo mkdir /shared
 
 wget -O /tmp/sd2_compile_dir_512.tgz https://inf2-exports.s3.us-east-2.amazonaws.com/sd2_compile_dir_512.tgz
 tar xf /tmp/sd2_compile_dir_512.tgz -C /shared
 rm -f /tmp/sd2_compile_dir_512.tgz
 # This image is built with the provided Dockerfile.aws_export
-docker run -d --device=/dev/neuron0 --name test1 -v /shared:/shared raphael31415/aws-inf2:1.0
+sudo docker run -d --device=/dev/neuron0 --name test1 -v /shared:/shared raphael31415/aws-inf2:1.0
 
 # wget -O /tmp/sd_neuron.tgz https://inf2-exports.s3.us-east-2.amazonaws.com/sd_neuron.tgz
 # tar xf /tmp/sd_neuron.tgz -C /shared
