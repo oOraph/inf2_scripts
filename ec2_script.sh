b3af08c498204406a5ccf472ba5d994afed867ab
Raphael <oOraph@users.noreply.github.com>
2023-08-18 10:29:52 +0200

    wip
    Signed-off-by: Raphael <oOraph@users.noreply.github.com>

--- ec2_script.sh
+++ ec2_script.sh
@@ -40,7 +40,7 @@ echo Install docker
 sudo apt-get install -y ca-certificates curl gnupg
 
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
