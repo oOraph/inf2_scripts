43d782108cc14d267d54814d88770d60655833a3
Raphael <oOraph@users.noreply.github.com>
2023-09-08 10:45:23 +0200

    add neuron precompiled models
    Signed-off-by: Raphael <oOraph@users.noreply.github.com>

--- ec2_script.sh
+++ ec2_script.sh
@@ -62,8 +62,15 @@ sudo mkdir -p /shared
 # This image is built with the provided Dockerfile.aws_export
 # sudo docker run -d --device=/dev/neuron0 --name test1 -v /shared:/shared raphael31415/aws-inf2:1.0
 
+# Model exported using a tweaked aws neuron sample notebook, removing the optimized attention
+# wget -O /tmp/sd2_compile_dir_512.tgz https://inf2-exports.s3.us-east-2.amazonaws.com/sd2_compile_dir_512_non_optimized_attn.tgz
+# sudo tar xf /tmp/sd2_compile_dir_512_non_optimized_attn.tgz -C /shared
+# sudo mv /shared/sd2_compile_dir_512_non_optimized_attn /shared/sd2_compile_dir_512
+# rm -f /tmp/sd2_compile_dir_512_non_optimized_attn.tgz
+# This image is built with the provided Dockerfile.aws_export
+# sudo docker run -d --device=/dev/neuron0 --name test1 -v /shared:/shared raphael31415/aws-inf2:1.0
 
-# Model exported using optimum
+# Model exported using optimum (without export NEURON_FUSE_SOFTMAX=1, e.g without optimized_attn)
 # optimum-cli export neuron --model stabilityai/stable-diffusion-2-1-base \
 #   --task stable-diffusion \
 #   --batch_size 1 \
@@ -72,9 +79,25 @@ sudo mkdir -p /shared
 #   --auto-cast matmul \
 #   --auto-cast-type bf16 \
 #   sd_neuron/
-wget -O /tmp/sd_neuron.tgz https://inf2-exports.s3.us-east-2.amazonaws.com/sd_neuron.tgz
-sudo tar xf /tmp/sd_neuron.tgz -C /shared
-rm -f /tmp/sd_neuron.tgz
+# wget -O /tmp/sd_neuron.tgz https://inf2-exports.s3.us-east-2.amazonaws.com/sd_neuron.tgz
+# sudo tar xf /tmp/sd_neuron.tgz -C /shared
+# rm -f /tmp/sd_neuron.tgz
+# This image is built with the provided Dockerfile.optimum_export
+# sudo docker run -d --rm --device=/dev/neuron0 --name test1 -v /shared:/shared raphael31415/aws-inf2-optimum:1.0
+
+# Model exported using optimum (with export NEURON_FUSE_SOFTMAX=1 beofre, e.g with optimized_attn)
+# optimum-cli export neuron --model stabilityai/stable-diffusion-2-1-base \
+#   --task stable-diffusion \
+#   --batch_size 1 \
+#   --height 512 `# height in pixels of generated image, eg. 512, 768` \
+#   --width 512 `# width in pixels of generated image, eg. 512, 768` \
+#   --auto-cast matmul \
+#   --auto-cast-type bf16 \
+#   sd_neuron/
+wget -O /tmp/sd_neuron.tgz https://inf2-exports.s3.us-east-2.amazonaws.com/sd_neuron_optimized_attn.tgz
+sudo tar xf /tmp/sd_neuron_optimized_attn.tgz -C /shared
+sudo mv /shared/sd_neuron_optimized_attn /shared/sd_neuron
+rm -f /tmp/sd_neuron_optimized_attn.tgz
 # This image is built with the provided Dockerfile.optimum_export
 sudo docker run -d --rm --device=/dev/neuron0 --name test1 -v /shared:/shared raphael31415/aws-inf2-optimum:1.0
 
